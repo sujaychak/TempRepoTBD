b2fc7cb306ba37d4de0b2de9a5506661b9e54f02
Chakraborty <sujay.a.chakraborty@capgemini.com>
2020-01-07 12:43:07 +0530

    Initial Commit

---
+++ DegreeOfArray/DegreeOfArray/NewBaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeOfArray
+{
+    public class NewBaseClass
+    {
+        public NewBaseClass() { }
+
+        public void GetValue()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ DegreeOfArray/DegreeOfArray/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Find the degree of an Array i.e. the max occurance of a single value
+ Find min length that would have same degree as the input array
+*/
+//css_include NewBaseClass.cs
+namespace DegreeOfArray
+{
+    class Program
+    {
+        internal struct ArrayStats
+        {
+            public int itemCount;
+            public int lastIdx;
+        }
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            Dictionary<int, ArrayStats> ar = new Dictionary<int, ArrayStats>();
+            Console.WriteLine("Enter the array, with elements separated by space");
+            string resp = Console.ReadLine();
+			NewBaseClass bc = new NewBaseClass();
+			bc.GetValue();
+            array.AddRange(resp.Split(' ').ToList());
+            int i = 0;
+            foreach (var item in array)
+            {
+                int temp = (int)item;
+                if (ar.Count > 0 && ar.ContainsKey(temp))
+                {
+                    ArrayStats ast = ar[temp];
+                    ast.itemCount++;
+                    ast.lastIdx = i;
+                    ar[temp] = ast;
+                }
+                else
+                {
+                    ar.Add((int)item, new ArrayStats { itemCount = 1, lastIdx = i });
+                }
+                i++;
+            }
+        }
+    }
+}
